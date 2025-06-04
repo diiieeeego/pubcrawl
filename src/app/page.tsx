2b2fa8ba2e5267e7bbf21b2e849679f61a7648c7
Jbilac <79338162+jbilac1@users.noreply.github.com>
2025-06-04 08:41:14 +0200

    pubcrawl start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <>
-      <div className="w-full h-screen bg-[url('/herobg.jpg')]"></div>
+      <div className="w-full h-screen bg-[url('/herobg.webp')]"></div>
     </>
   );
 }
