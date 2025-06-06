f18d560e3b22d5578c18cde304198e28ed6b8f92
Jbilac <79338162+jbilac1@users.noreply.github.com>
2025-06-06 12:57:06 +0200

    text-neutral-50

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
-        <main className="">
+        <main className=" text-neutral-50">
         {children}
         </main>
         <Footer />
