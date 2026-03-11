c70c6a401647c773c7cf2b95e4030a8e741e328e
Jbilac <79338162+jbilac1@users.noreply.github.com>
2026-03-11 23:38:15 +0100

    sitemap robots

---
+++ src/app/sitemap.ts
@@ -0,0 +1,16 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  
+  const baseUrl = 'https://pubcrawlzadar.com'
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    
+  ]
+}
